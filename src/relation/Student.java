e0336908ff44f93f36b0d6d2bbfd35b096d3d7fd
Yourmei <405140692@qq.com>
2019-01-23 21:31:32 +0800

    bean的关系--依赖
    1、spring允许用户通过depends-on属性设定bean的前置依赖的bean，前置依赖的bean会在本bean实例化之前创建好。
    2、如果前置依赖与多个bean，则可以通过逗号，空格的方式配置bean的名称

---
+++ src/relation/Student.java
@@ -0,0 +1,24 @@
+package relation;
+
+public class Student {
+
+	private Address address;
+	private String name;
+	
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Student [address=" + address + ", name=" + name + "]";
+	}
+}
